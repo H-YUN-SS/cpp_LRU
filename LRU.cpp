063957bfbeaf2578c6efa724ee84b0a3a0d1f45a
H-YUN-SS <920563885@qq.com>
2026-03-18 22:27:18 +0800

    put()方法

--- LRU.cpp
+++ LRU.cpp
@@ -30,6 +30,25 @@ public:
         out=it->second->second;
         return true;
     }
+    
+    void put(const K& key, V value)
+    {
+        auto it = index_.find(key);
+        if (it != index_.end())
+        {
+            it->second->second = std::move(value);
+            items_.splice(items_.begin(), items_, it->second);
+            return;
+        }
+        if (items_.size() == cap_)
+        {
+            auto& old_item = items_.back();
+            index_.erase(old_item.first);
+            items_.pop_back();
+        }
+        items_.emplace_front(key, std::move(value));
+        index_[items_.front().first] = items_.begin();
+    }
 private:
     size_t cap_;
     std::list<std::pair<K,V>>items_;
