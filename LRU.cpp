bce4752af6ae23b6700b0ef184ac75ce8e417c9f
H-YUN-SS <920563885@qq.com>
2026-03-19 11:30:50 +0800

    辅助方法+debug打印+主函数定义

--- LRU.cpp
+++ LRU.cpp
@@ -49,6 +49,61 @@ public:
         items_.emplace_front(key, std::move(value));
         index_[items_.front().first] = items_.begin();
     }
+
+    void erase(const K&key)
+    {
+        auto it=index_.find(key);
+        if(it==index_.end())
+        {
+            return;
+        }
+        items_.erase(it->second);
+        index_.erase(it);
+    }
+
+    void clear()
+    {
+        items_.clear();
+        index_.clear();
+    }
+    
+    size_t size() const
+    {
+        return items_.size();
+    }
+
+    size_t capacity() const
+    {
+        return cap_;
+    }
+
+    void resize(size_t newcap)
+    {
+        if(newcap==0)
+        {
+            throw std::invalid_argument("new capecity cant't ==0 ");
+        }
+        cap_=newcap;
+
+        while(items_>cap_)
+        {
+            auto &old_item=items_.back();
+            index_.erase(old_item.first);
+            items_.pop_back();
+        }
+    }
+
+    void debug_print()const
+    {
+        std::cout<<"[MRU->LRU]";
+        for(auto it=items_.begin();it!=items_.end();it++)
+        {
+            std::cout<<"("<<it->first<<":"<<it->second<<")";
+        }
+        std::cout<<"\n";
+    }
+
+
 private:
     size_t cap_;
     std::list<std::pair<K,V>>items_;
@@ -58,3 +113,14 @@ private:
 
 
 
+int main()
+{
+    LRUCache<int,std::string>cache(3);
+    std::cout<<"===插入三个元素==="<<std::endl;
+    cache.put(1,"A");
+    cache.put(2,"B");
+    cache.put(3,"C");
+    cache.debug_print();
+
+    
+}
